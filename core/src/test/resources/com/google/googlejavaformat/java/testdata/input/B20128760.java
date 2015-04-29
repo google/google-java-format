d76758419c842cacfd063c8dbff66bc2e923f612
cushon <cushon@google.com>
2015-04-29 13:02:44 -0700

    Add tests for cases where not wrapping produces a very small rectangle overlap. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=92381662

---
+++ core/src/test/resources/com/google/googlejavaformat/java/testdata/input/B20128760.java
@@ -0,0 +1,73 @@
+class B20128760 {
+  void f() {
+    x =
+        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx(
+            xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx);
+
+    XxxxxxxxxXxx.<Xxxxxx, Xxxxxx>xxxxxxx()
+        .xxx(XxxxxxxxxXxxXxxx.Xxxxx.XXXXX, "Xxxxx xxxxxxxxx xxxxx")
+        .xxx(
+            XxxxxxxxxXxxXxxx.Xxxxx.XXXXXxXXXXX,
+            XxxxXxxxxxxxxx.xxxXxxxxxxxxXxxxxXxxxxXxXxxxxx(Xxxxxxxxxx.xxxXxxxXxxxx(x)))
+        .xxxxx();
+
+    xxxxxxXxxx(
+            xxx
+                .xxxXxXxxxxxxx(
+                    new XxxxxxxxxXxxxxxxx.Xxxxxxx()
+                        .xxxxXxxxXxx(
+                            Xxxxx.xxxXxxxxXxxx(
+                                new XxxxXx(
+                                    xxxxxxxxx1.xxxXxxxXx().xxxXxxxx()
+                                        + xxxxxxxxx2.xxxXxxxXx().xxxXxxxx())))
+                        .xxxxx())
+                .xxxXxxxxxx())
+        .xxxxxxxxXxxxxxx();
+
+    XXxxxx<Xxxx, XxxxxxxXxxxxxxxxxxXxxxxxxxx> xxxxxxxxxXxxXxxXxXxxxxxxxXx =
+        XX
+            .xxxxXxxXxxx2(
+                "XxxxXxxxxxxxXxxxXxxxXxxxxxxxXxxxXxxXxxxxxxXxxxxxxxx",
+                xxxxxxxxXxxxXxXxxxxxxxXx,
+                xxxxxxxxXxxxXxxxxXxxxXxXxxxxxxxXx)
+            .xxxxxxxxXx(
+                "XxxxxxxxXxxxxxxxxXxx",
+                xxxxxxxxXxxxxxxxxXxxXx,
+                XX.xxxxxXx(XX.xxxxx(), XX.xxxxxx(XxxxxxxXxxxxxxxxxxXxxxxxxxx.xxxxx)));
+
+    xxXxxxxxxxXxxxXxxx =
+        (XxxxXxxx)
+            xxxxxxxxXxxxxxxxxx.xxx()
+                .xxxxxXxxxxxx
+                .xxxXxxxxxxx(xxxxxxxxXxxxxxxxxx.xxx().xxxxXxxxx.xxxXxxxxxxXxxx() - 1)
+                .xxxXxxx()
+                .xxxxXxxxXxXx(X.xx.xxxxxxxxxxxxxxxxxxxxxxxxxxx);
+
+    xxxxxxXxxx(
+            xxxxxxxxxx
+                .xxxXxxxXxxxxXxxxxxxxxxxXxxxXxxxXxxxxXxxx(XXXxXXXXxXX, XXXXXXxXXXxXXXXxXX, XXXXXxXX)
+                .xxxxxxxxxxxXxxxXxxxXxxxxx())
+        .xxxxxxxxXxxxxxx(
+            xxxxXxxxxxxxxxxXxxxXxxxXxxxx(xxxx1Xxxx, Xxxxx.XXxXXX),
+            xxxxXxxxxxxxxxxXxxxXxxxXxxxx(xxxx2Xxxx, Xxxxx.XXxXXX));
+
+    xxxx(
+        xxxx(
+            xxxx(
+                xxxx(
+                    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx))));
+  }
+
+  private static class GenerateDashboardRowFn
+      extends
+          XX.XxXx<
+              Xxxx<
+                  Xxxx,
+                  Xxxxx2.XxxXxxxXxxxxxxxxxx<XxxxxxxxXxxx, Xxxx<XxxxxxxxXxxx, XxxxxxxXxxxxxxxx>>>,
+              Xxxx<Xxxx, XxxxxxxXxxxxxxxxxxXxxxxxxxx>> {}
+
+  public class XxxxxxxXxxxxxxxXxxxXxxxxXxxxxxx<X>
+      extends
+          XxxxxxxXxxxxxxxXxxxXxxxxXxxxxxx<
+              XxxxxxxXxxxxxxxXxxxXxxxxXxxxxxx<X>, XxxxXxxxxxxXxxxxxxxx<? extends Xxxxxxxx<X>>> {}
+}
