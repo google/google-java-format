67c6a53a94e16c97f8dac98eae79df4818f8360b
cushon <cushon@google.com>
2015-05-20 15:29:28 -0700

    Add regression test
    No behaviour changes; the fix fell out of
    77b4cb7b6f27e54375a6a0a0bfcdb497b370e12b
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=94131643

---
+++ core/src/test/resources/com/google/googlejavaformat/java/testdata/input/B20915776.java
@@ -0,0 +1,10 @@
+class B20915776 {
+  @Inject
+    void ChunkAnalysis(
+    // TODO(user) ...
+    @Parent DatasetSource supporters,
+    @Child DatasetSource children,
+    @Parent Provider<SupportsDecider> parentSupportsDecider,
+    @Child Provider<SupportsDecider> childSupportsDecider) {
+  }
+}
