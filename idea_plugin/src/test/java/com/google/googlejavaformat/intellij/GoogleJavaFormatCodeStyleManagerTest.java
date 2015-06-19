bbcce560828016f9146b41050c1cfe50fbf953ff
bcsf <bcsf@google.com>
2015-06-18 18:10:31 -0700

    IntelliJ IDEA plugin for google-java-format.
    Adds a new menu item "Reformat with google-java-format" into the Code
    menu. When the user clicks this menu item, we will substitute the
    default CodeStyleManager with an implementation which will perform
    google-java-format on .java files.
    
    From then on, even clicking "Reformat code" will use this implementation
    on .java files, rather than IJ's CodeStyleManager, until IJ is
    restarted.
    
    Follow-up task is to let the user configure which CodeStyleManager
    they'd like to be active, as well as persist this setting on a
    per-project basis.
    
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=96368327

---
+++ idea_plugin/src/test/java/com/google/googlejavaformat/intellij/GoogleJavaFormatCodeStyleManagerTest.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.googlejavaformat.intellij;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.fileTypes.StdFileTypes;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.codeStyle.CodeStyleManager;
+import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+
+/**
+ * Tests for {@link GoogleJavaFormatCodeStyleManager}.
+ *
+ * @author bcsf@google.com (Brian Chang)
+ */
+public class GoogleJavaFormatCodeStyleManagerTest extends LightCodeInsightFixtureTestCase {
+
+  @Override
+  protected void setUp() throws Exception {
+    if (getClass().getClassLoader() instanceof URLClassLoader) {
+      for (URL url : ((URLClassLoader) getClass().getClassLoader()).getURLs()) {
+        if (url.getPath().matches(".*/ecj-\\d(\\.\\d)*\\.jar")) {
+          System.err.println(
+              "If you see a SecurityException while running this test from within IJ,\n"
+                  + "unfortunately you'll need to somehow remove ecj.jar from your classpath,\n"
+                  + "as it will cause a cert conflict with org.eclipse.jdt.core.jar.\n"
+                  + "e.g. if " + url.getPath() + " is in IDEA_HOME/lib, temporarily relocate it");
+        }
+      }
+    }
+    super.setUp();
+  }
+
+  public void testFormatFile() {
+    CodeStyleManager codeStyleManager = CodeStyleManager.getInstance(getProject());
+    final GoogleJavaFormatCodeStyleManager googleJavaFormat =
+        new GoogleJavaFormatCodeStyleManager(codeStyleManager);
+    myFixture.configureByText(
+        StdFileTypes.JAVA,
+        "public class Test {public static void main(String[]args){System.out.println();}}");
+    final int startOffset = 0;
+    final int endOffset = myFixture.getFile().getTextLength();
+    ApplicationManager.getApplication().runWriteAction(new Runnable() {
+      @Override
+      public void run() {
+        googleJavaFormat.reformatText(myFixture.getFile(), startOffset, endOffset);
+      }
+    });
+    myFixture.checkResult(join(
+        "public class Test {",
+        "  public static void main(String[] args) {",
+        "    System.out.println();",
+        "  }",
+        "}"));
+  }
+
+  public void testDontFormatNonJavaFile() {
+    CodeStyleManager codeStyleManager = CodeStyleManager.getInstance(getProject());
+    final GoogleJavaFormatCodeStyleManager googleJavaFormat =
+        new GoogleJavaFormatCodeStyleManager(codeStyleManager);
+    myFixture.configureByText(
+        StdFileTypes.PLAIN_TEXT,
+        "public class Test {public static void main(String[]args){System.out.println();}}");
+    final int startOffset = 0;
+    final int endOffset = myFixture.getFile().getTextLength();
+    ApplicationManager.getApplication().runWriteAction(new Runnable() {
+      @Override
+      public void run() {
+        googleJavaFormat.reformatText(myFixture.getFile(), startOffset, endOffset);
+      }
+    });
+    myFixture.checkResult(
+        "public class Test {public static void main(String[]args){System.out.println();}}");
+  }
+
+  public void testFormatRanges() {
+    CodeStyleManager codeStyleManager = CodeStyleManager.getInstance(getProject());
+    final GoogleJavaFormatCodeStyleManager googleJavaFormat =
+        new GoogleJavaFormatCodeStyleManager(codeStyleManager);
+    String content = join(
+        "public class Test {",
+        "  int a=0;",
+        "  int b=1;",
+        "  int c=2;",
+        "}");
+    myFixture.configureByText(StdFileTypes.JAVA, content);
+    int aLineStart = content.indexOf("int a");
+    int aLineEnd = content.indexOf("\n", aLineStart);
+    int cLineStart = content.indexOf("int c");
+    int cLineEnd = content.indexOf("\n", cLineStart);
+    final ImmutableList<TextRange> ranges =
+        ImmutableList.of(new TextRange(aLineStart, aLineEnd), new TextRange(cLineStart, cLineEnd));
+    ApplicationManager.getApplication().runWriteAction(new Runnable() {
+      @Override
+      public void run() {
+        googleJavaFormat.reformatText(myFixture.getFile(), ranges);
+      }
+    });
+    myFixture.checkResult(join(
+        "public class Test {",
+        "  int a = 0;",
+        "  int b=1;",
+        "  int c = 2;",
+        "}"));
+  }
+
+  private static String join(String... lines) {
+    return Joiner.on("\n").join(lines);
+  }
+}
