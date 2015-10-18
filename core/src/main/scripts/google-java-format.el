f3acef6984982bb6bca7d600e2420b7406d839e4
Syohei YOSHIDA <syohex@gmail.com>
2015-10-19 08:24:37 +0900

    Specify minimum Emacs version
    lexical-binding was introduced since Emacs 24.

--- core/src/main/scripts/google-java-format.el
+++ core/src/main/scripts/google-java-format.el
@@ -2,6 +2,8 @@
 ;;
 ;; Copyright 2015 Google, Inc. All Rights Reserved.
 ;;
+;; Package-Requires: ((emacs "24"))
+;;
 ;; Licensed under the Apache License, Version 2.0 (the "License");
 ;; you may not use this file except in compliance with the License.
 ;; You may obtain a copy of the License at
