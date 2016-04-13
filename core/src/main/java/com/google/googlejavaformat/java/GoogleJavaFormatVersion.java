f8a2688f012bbc10f30cce5234d1b5370f4a4865
Liam Miller-Cushon <cushon@google.com>
2016-04-12 17:41:49 -0700

    1.0 release

--- core/src/main/java/com/google/googlejavaformat/java/GoogleJavaFormatVersion.java
+++ core/src/main/java/com/google/googlejavaformat/java/GoogleJavaFormatVersion.java
@@ -15,5 +15,5 @@
 package com.google.googlejavaformat.java;
 
 public class GoogleJavaFormatVersion {
-  public static final String VERSION = "0.dev";
+  public static final String VERSION = "1.0";
 }
