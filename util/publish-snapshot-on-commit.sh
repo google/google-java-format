f8ed3f156304a4dfe7736a9401a9a090e0ad8be3
Liam Miller-Cushon <cushon@google.com>
2016-04-23 16:44:44 -0700

    Auto-publish snapshots

---
+++ util/publish-snapshot-on-commit.sh
@@ -0,0 +1,12 @@
+# https://github.com/google/dagger/blob/master/util/publish-snapshot-on-commit.sh
+
+if [ "$TRAVIS_REPO_SLUG" == "google/google-java-format" ] && \
+   [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && \
+   [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
+   [ "$TRAVIS_BRANCH" == "master" ]; then
+  echo -e "Publishing maven snapshot...\n"
+
+  mvn clean source:jar deploy --settings="util/settings.xml" -DskipTests=true -Dinvoker.skip=true -Dmaven.javadoc.skip=true
+
+  echo -e "Published maven snapshot"
+fi
