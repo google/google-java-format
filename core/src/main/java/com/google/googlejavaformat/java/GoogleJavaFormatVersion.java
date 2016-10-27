069691880726c387e579f4d7e5ff3bbf9ae448ac
Christian Stein <sormuras@gmail.com>
2016-10-27 23:49:35 +0200

    Refactored to interface, version constant updated.
    Made type `GoogleJavaFormatVersion` an interface - not instantiable. Removed redundant modifiers and updated the version string reflect  current setting in `pom.xml`.

--- core/src/main/java/com/google/googlejavaformat/java/GoogleJavaFormatVersion.java
+++ core/src/main/java/com/google/googlejavaformat/java/GoogleJavaFormatVersion.java
@@ -14,6 +14,6 @@
 
 package com.google.googlejavaformat.java;
 
-public class GoogleJavaFormatVersion {
-  public static final String VERSION = "1.0";
+public interface GoogleJavaFormatVersion {
+  String VERSION = "1.2-SNAPSHOT";
 }
