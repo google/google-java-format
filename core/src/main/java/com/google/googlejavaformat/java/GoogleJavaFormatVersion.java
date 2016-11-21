0af7a7a4b7174ac434cedf1dd17c94fd6c1d16cd
cushon <cushon@google.com>
2016-11-21 11:32:54 -0800

    Don't crash on type annotated varargs parameters
    Fixes #95
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=139810602

--- core/src/main/java/com/google/googlejavaformat/java/GoogleJavaFormatVersion.java
+++ core/src/main/java/com/google/googlejavaformat/java/GoogleJavaFormatVersion.java
@@ -14,9 +14,6 @@
 
 package com.google.googlejavaformat.java;
 
-import com.google.common.base.MoreObjects;
-
 public class GoogleJavaFormatVersion {
-  public static final String VERSION =
-      MoreObjects.firstNonNull(Main.class.getPackage().getImplementationVersion(), "dev");
+  public static final String VERSION = "1.0";
 }
