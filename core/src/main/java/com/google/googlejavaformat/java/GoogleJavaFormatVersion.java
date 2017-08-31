bd41f95e6ed171c048cdd663b19d8b83f5474f76
Christian Stein <sormuras@gmail.com>
2017-08-31 06:10:56 +0200

    Set version to 1.5-SNAPSHOT

--- core/src/main/java/com/google/googlejavaformat/java/GoogleJavaFormatVersion.java
+++ core/src/main/java/com/google/googlejavaformat/java/GoogleJavaFormatVersion.java
@@ -15,5 +15,5 @@
 package com.google.googlejavaformat.java;
 
 public class GoogleJavaFormatVersion {
-  public static final String VERSION = "1.0";
+  public static final String VERSION = "1.5-SNAPSHOT";
 }
