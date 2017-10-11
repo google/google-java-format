04314d8821b864e9fa3ea4352642e673b6c4effd
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2017-10-10 18:05:15 -0700

    Replace JDK singleton lists with ImmutableList
    Fixes #133
    
    MOE_MIGRATED_REVID=171763302

--- core/src/main/java/com/google/googlejavaformat/java/Formatter.java
+++ core/src/main/java/com/google/googlejavaformat/java/Formatter.java
@@ -38,7 +38,6 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import org.openjdk.javax.tools.Diagnostic;
 import org.openjdk.javax.tools.DiagnosticCollector;
@@ -204,7 +203,7 @@ public final class Formatter {
    * @throws FormatterException if the input string cannot be parsed
    */
   public String formatSource(String input) throws FormatterException {
-    return formatSource(input, Collections.singleton(Range.closedOpen(0, input.length())));
+    return formatSource(input, ImmutableList.of(Range.closedOpen(0, input.length())));
   }
 
   /**
