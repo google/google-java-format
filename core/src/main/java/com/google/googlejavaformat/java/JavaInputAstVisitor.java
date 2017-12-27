3b1f508e7cbe643242f0602a299437afc8705be1
Kitonjics Marko <kitonjic95@gmail.com>
2017-12-28 00:55:54 +0100

    Fixed the formatting of statements without braces where there is a comment on the same line

--- core/src/main/java/com/google/googlejavaformat/java/JavaInputAstVisitor.java
+++ core/src/main/java/com/google/googlejavaformat/java/JavaInputAstVisitor.java
@@ -1472,7 +1472,6 @@ public final class JavaInputAstVisitor extends TreePathScanner<Void, Void> {
     return null;
   }
 
-
   @Override
   public Void visitMemberSelect(MemberSelectTree node, Void unused) {
     sync(node);
@@ -2022,7 +2021,7 @@ public final class JavaInputAstVisitor extends TreePathScanner<Void, Void> {
         break;
       default:
         builder.open(plusTwo);
-        builder.breakOp(" ");
+        builder.breakOp(UNIFIED, " ", plusTwo);
         scan(node, null);
         builder.close();
     }
