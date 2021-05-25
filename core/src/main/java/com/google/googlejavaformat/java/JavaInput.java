fbade77ff2d04007983685d91870439631c9c115
Cameron Samak <csamak@google.com>
2021-05-25 07:46:51 -0700

    Simplify characterRangeToTokenRange by taking a Range instead of an offset + length.
    It had some extra logic (e.g. logic to check length < 0, which was already impossible since ranges were already created).
    Leaving the error message the same since the cli options include "offset" and "length"
    
    PiperOrigin-RevId: 375702715

--- core/src/main/java/com/google/googlejavaformat/java/JavaInput.java
+++ core/src/main/java/com/google/googlejavaformat/java/JavaInput.java
@@ -557,33 +557,28 @@ public final class JavaInput extends Input {
   }
 
   /**
-   * Convert from an offset and length flag pair to a token range.
+   * Convert from a character range to a token range.
    *
-   * @param offset the {@code 0}-based offset in characters
-   * @param length the length in characters
+   * @param characterRange the {@code 0}-based {@link Range} of characters
    * @return the {@code 0}-based {@link Range} of tokens
-   * @throws FormatterException if offset + length is outside the file
+   * @throws FormatterException if the upper endpoint of the range is outside the file
    */
-  Range<Integer> characterRangeToTokenRange(int offset, int length) throws FormatterException {
-    int requiredLength = offset + length;
-    if (requiredLength > text.length()) {
+  Range<Integer> characterRangeToTokenRange(Range<Integer> characterRange)
+      throws FormatterException {
+    if (characterRange.upperEndpoint() > text.length()) {
       throw new FormatterException(
           String.format(
               "error: invalid length %d, offset + length (%d) is outside the file",
-              length, requiredLength));
+              characterRange.upperEndpoint() - characterRange.lowerEndpoint(),
+              characterRange.upperEndpoint()));
     }
-    if (length < 0) {
-      return EMPTY_RANGE;
-    }
-    if (length == 0) {
-      // 0 stands for "format the line under the cursor"
-      length = 1;
-    }
+    // empty range stands for "format the line under the cursor"
+    Range<Integer> nonEmptyRange =
+        characterRange.isEmpty()
+            ? Range.closedOpen(characterRange.lowerEndpoint(), characterRange.lowerEndpoint() + 1)
+            : characterRange;
     ImmutableCollection<Token> enclosed =
-        getPositionTokenMap()
-            .subRangeMap(Range.closedOpen(offset, offset + length))
-            .asMapOfRanges()
-            .values();
+        getPositionTokenMap().subRangeMap(nonEmptyRange).asMapOfRanges().values();
     if (enclosed.isEmpty()) {
       return EMPTY_RANGE;
     }
@@ -664,12 +659,9 @@ public final class JavaInput extends Input {
   public RangeSet<Integer> characterRangesToTokenRanges(Collection<Range<Integer>> characterRanges)
       throws FormatterException {
     RangeSet<Integer> tokenRangeSet = TreeRangeSet.create();
-    for (Range<Integer> characterRange0 : characterRanges) {
-      Range<Integer> characterRange = characterRange0.canonical(DiscreteDomain.integers());
+    for (Range<Integer> characterRange : characterRanges) {
       tokenRangeSet.add(
-          characterRangeToTokenRange(
-              characterRange.lowerEndpoint(),
-              characterRange.upperEndpoint() - characterRange.lowerEndpoint()));
+          characterRangeToTokenRange(characterRange.canonical(DiscreteDomain.integers())));
     }
     return tokenRangeSet;
   }
